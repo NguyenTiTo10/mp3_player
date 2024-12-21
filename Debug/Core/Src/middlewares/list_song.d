6fafc39506cea98026fbcaec865095c5eb45fd13
nguyentito10 <tientoan91002003@gmail.com>
2024-12-21 19:09:04 +0700

    Update: file .gitignore

--- Debug/Core/Src/middlewares/list_song.d
+++
@@ -1,3 +0,0 @@
-Core/Src/middlewares/list_song.o: ../Core/Src/middlewares/list_song.c \
- C:/Users/Papa/Desktop/VXL/final_project/mp3_player/Core/Inc/middlewares/list_song.h
-C:/Users/Papa/Desktop/VXL/final_project/mp3_player/Core/Inc/middlewares/list_song.h:
